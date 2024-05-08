b6330de8828f143f2396f88a90938408c2fad59c
yll0M <bosmacdonald@gmail.com>
2024-05-08 02:57:07 -0400

    Added cool stuff

--- HulloWorld.py/Wrold.py
+++ HulloWorld.py/Wrold.py
@@ -1,1 +1,17 @@
+import random
+def displaynumber(number):
+    print("Your number is "+str(number))
+
+
 print ("Hullo World")
+displaynumber(random.randint(1,9999))
+displaynumber(input ("Type a random number"))
+name=input ("What is your name?")
+print ("Hello "+name)
+response=input ("Type end to close or type joke for a joke")
+while not response.lower()=="end":
+    if response.lower()== "joke":
+        print ("You")
+    else:
+        print ("Invalid input")
+    response=input ("Type end to close or type joke for a joke")
